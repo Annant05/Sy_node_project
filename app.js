e526595585c0b45d0cf5ce5bc3dc32c4903e5035
annant <annantguptaneema@gmail.com>
2018-12-27 01:18:30 +0530

    a lot of progress. dynamic outputs, create stack using name and button.

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
 // all requires and declarations
-// const AWS = require('aws-sdk');
 const express = require("express"), app = express(); // creating express server
 const path = require('path');
 const request = require("request");
 const bodyParser = require("body-parser");  // used bodyparser to get data from all the field in form
-// const awsCredentials = (require("./config/config")).getAWS_JSONCredentials();
 const CFNfile = require('./launch_cloudformation');
-// AWS.config.update(awsCredentials);
-// // console.log(awsCredentials);
-//
-// const cloudformation = new AWS.CloudFormation();
-// // const templateString = JSON.stringify(require("./config/createvpc"));
-// // console.log(templateString);
-//
-// var params = {
-//     StackName: 'vpcCreatedFromNodeSDK'
-// };
-//
-// function getStackOutputs(callback) {
-//     cloudformation.describeStacks(params, function (err, data) {
-//
-//         if (err) console.log(err, err.stack); // an error occurred
-//         else {
-//             // console.log(data);
-//             // console.log((data['Stacks'][0])['Outputs'][0]);
-//             // let temp = JSON.parse(data);
-//             // console.log(temp);
-//             // noinspection UnnecessaryLocalVariableJS
-//             let outputs = (data['Stacks'][0])['Outputs'][0];
-//             callback(err, outputs);
-//         }           // successful response
-//     });
-// }
 
 
 // Declaration related to servers
@@ -59,13 +31,6 @@ app.get('/index', function (req, res) {
     res.render('index');
 });
 
-app.get('/tableshow', function (req, res) {
-    CFNfile.getOutputsCFN(function (data) {
-        console.log(data);
-        res.end(JSON.stringify(data));
-    });
-
-});
 
 app.get('/coming', function (req, res) {
     res.render('coming');
@@ -87,3 +52,26 @@ app.listen(PORT, function (err) {
 });
 
 console.log('Server-side code running');
+
+app.get('/launchstack', function (req, res) {
+    res.render("launchstack");
+});
+
+app.post('/launchstack', function (req, res) {
+    console.log(req.body.stackName);
+    CFNfile.createSTK(req.body.stackName);
+});
+
+app.get('/tableshow', function (req, res) {
+
+    CFNfile.getStackOutputs(function (data) {
+        // console.log(data);
+        // res.end(JSON.stringify(data));
+        res.render("tableshow", {outdata});
+        // data.forEach(function(element,index) {
+        //       // console.log("element " + index + element);
+        //       console.log("data is : " + element['OutputKey'] + " val : "  + element ['OutputValue']);
+        // });
+    });
+
+});
--- launch_cloudformation.js
+++ launch_cloudformation.js
@@ -9,51 +9,63 @@ AWS.config.update(awsCredentials);
 // console.log(awsCredentials);
 
 const cloudformation = new AWS.CloudFormation();
-const templateString = JSON.stringify(require("./config/createvpc"));
+const templateString = JSON.stringify(require("./config/LoadBal2webEc2db2vpc"));
 // console.log(templateString);
 
-var params = {
-    StackName: 'vpcCreatedFromNodeSDK'
+// let stackName = "";
+
+let mainbody = {
+    stackName: "",
+
+    createSTK: function (stackName) {
+        mainbody.stackName = stackName;
+
+        const params = {
+            StackName: mainbody.stackName, /* required */
+            // DisableRollback: false,
+            EnableTerminationProtection: false,
+            OnFailure: "DO_NOTHING",
+            // RoleARN: 'arn:aws:iam::805746249177:role/Allow_all_resources_to_cloudformation',
+            TemplateBody: (templateString),
+        };
+        console.log(params);
+
+        // remove comments from below lines when used in producation enviroment. this lines can cause charges.
+        // cloudformation.createStack(params, function (err, data) {
+        //     if (err) console.log(err, err.stack); // an error occurred
+        //     else console.log(data);           // successful response
+        // });
+        // listexp()
+    },
+
+    getStackOutputs: function (callback) {
+        const params = {
+            StackName: mainbody.stackName
+        };
+        cloudformation.describeStacks(params, function (err, data) {
+
+            if (err) console.log(err, err.stack); // an error occurred
+            else {
+                // console.log(data);
+                // console.log((data['Stacks'][0])['Outputs'][0]);
+                // let temp = JSON.parse(data);
+                // console.log(temp);
+                // noinspection UnnecessaryLocalVariableJS
+                let outputs = (data['Stacks'][0])['Outputs'];
+                callback(outputs);
+            }           // successful response
+        });
+    }
+
+
 };
 
-const getOutputsCFN = function getStackOutputs(callback) {
-    cloudformation.describeStacks(params, function (err, data) {
+module.exports = mainbody;
 
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-            // console.log(data);
-            // console.log((data['Stacks'][0])['Outputs'][0]);
-            // let temp = JSON.parse(data);
-            // console.log(temp);
-            // noinspection UnnecessaryLocalVariableJS
-            let outputs = (data['Stacks'][0])['Outputs'][0];
-            callback(outputs);
-        }           // successful response
-    });
-};
-
-module.exports = {getOutputsCFN};
 
 // createSTK();
 
-// function createSTK() {
-//
-//     const params = {
-//         StackName: 'vpcCreatedFromNodeSDK', /* required */
-//
-//         DisableRollback: false,
-//         EnableTerminationProtection: false,
-//         // OnFailure: "DO_NOTHING",
-//         // RoleARN: 'arn:aws:iam::805746249177:role/Allow_all_resources_to_cloudformation',
-//         TemplateBody: (templateString),
-//     };
-//     // console.log(params);
-//     cloudformation.createStack(params, function (err, data) {
-//         if (err) console.log(err, err.stack); // an error occurred
-//         else console.log(data);           // successful response
-//     });
-//     listexp()
-// }
+// function
 
 // listexp();
 //
